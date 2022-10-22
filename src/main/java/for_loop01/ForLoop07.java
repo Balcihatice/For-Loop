2489a6d7f6cf6e1be7d8dc9071d223cfd2799cda
Balcihatice <aciceaybike@gmail.com>
2022-10-23 00:14:10 +0300

    yildiz sekiller

---
+++ src/main/java/for_loop01/ForLoop07.java
@@ -0,0 +1,21 @@
+package for_loop01;
+
+public class ForLoop07 {
+    public static void main(String[] args) {
+ /*      *
+         **
+         ***
+         ****       sekli olusturan kodu yaziniz
+   */
+
+
+        for (int i= 1; i<=4; i++){
+
+            for (int k= 1; k<=i; k++){
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
